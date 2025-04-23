e4b2ff61a51a0664119d976a8f9b36e1ab29f6c1
Mduduzi Mdlalose <Mduduzi.Mdlalose@standardbank.co.za>
2025-04-23 20:57:26 +0200

    Initial commit for OTLP Logger app

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# otlp-logger-openshift
+# OTLP Logger for OpenShift
+
+This Node.js application sends logs to a Tempo distributor's OTLP endpoint at `http://tempo-distributor:4317/v1/logs` in an OpenShift cluster.
+
+## Deployment
+Deploy this application to OpenShift using the BuildConfig and Deployment YAML provided in the documentation.
---
+++ index.js
@@ -0,0 +1,48 @@
+const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
+const { LogRecordProcessor, SimpleLogRecordProcessor } = require('@opentelemetry/sdk-logs');
+const { OTLPLogExporter } = require('@opentelemetry/exporter-logs-otlp-http');
+const { LoggerProvider } = require('@opentelemetry/sdk-logs');
+
+// Set up diagnostics for debugging
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+
+// Configure OTLP exporter for logs
+const exporter = new OTLPLogExporter({
+  url: 'http://tempo-distributor:4317/v1/logs', // OTLP HTTP endpoint for logs in OpenShift namespace
+  headers: {},
+});
+
+// Set up the logger provider
+const loggerProvider = new LoggerProvider();
+loggerProvider.addLogRecordProcessor(new SimpleLogRecordProcessor(exporter));
+
+// Get a logger instance
+const logger = loggerProvider.getLogger('otlp-logger');
+
+// Function to emit logs periodically
+function emitLogs() {
+  console.log('Emitting a log record...');
+  logger.emit({
+    severityText: 'INFO',
+    body: 'This is a sample log message sent to Tempo via OTLP',
+    attributes: {
+      'log.type': 'sample',
+      'source': 'nodejs-app'
+    }
+  });
+
+  // Schedule the next log emission
+  setTimeout(emitLogs, 10000); // Every 10 seconds
+}
+
+// Start emitting logs
+console.log('Starting log emission to OTLP endpoint...');
+emitLogs();
+
+// Keep the application running
+process.on('SIGINT', () => {
+  loggerProvider.shutdown().then(() => {
+    console.log('Logger provider shut down.');
+    process.exit(0);
+  });
+});
